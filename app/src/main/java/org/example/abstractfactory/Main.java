5c76a11575f3d968b6bd6f4a390697ab3420d813
sasas <aldiyardaniyal@gmail.com>
2026-09-23 00:36:38 +0500

    two main entry points

---
+++ app/src/main/java/org/example/abstractfactory/Main.java
@@ -0,0 +1,12 @@
+package org.example.abstractfactory;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NotificationFactory factory =
+                new AndroidNotificationFactory();
+
+        Client client = new Client(factory);
+        client.run("hello from notification system");
+    }
+}
---
+++ app/src/main/java/org/example/factorymethod/Main.java
@@ -0,0 +1,9 @@
+package org.example.factorymethod;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+}
