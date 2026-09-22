b36600c8d6ea487ac68ef44f58e3b18e404b9a34
sasas <aldiyardaniyal@gmail.com>
2026-09-22 20:54:09 +0500

    app update

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,21 @@
 package org.example;
 
-import org.example.factorymethod.Client;
-
 public class App {
 
     public static void main(String[] args) {
-        Client client=new Client();
-        client.run();
+
+        org.example.factorymethod.Client factoryMethodClient=new org.example.factorymethod.Client();
+
+        factoryMethodClient.run();
+
+
+
+
+
+        org.example.abstractfactory.NotificationFactory factory=new org.example.abstractfactory.AndroidNotificationFactory();
+
+        org.example.abstractfactory.Client abstractFactoryClient=new org.example.abstractfactory.Client(factory);
+
+        abstractFactoryClient.run("hello from notification system");
     }
 }
